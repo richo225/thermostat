572c4eba3f974abc0f72e9a34dcc728183c9d173
Sara <sarah.alalaya@gmail.com>
2016-08-03 13:43:00 +0300

    Temperature increases by 1 when up button is pressed

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -1,11 +1,23 @@
-// SPECIFICATION 1:
-// Thermostat starts at 20 degrees
+
 
 describe('Thermostat', function(){
   var thermostat;
+  // SPECIFICATION 1:
+  // Thermostat starts at 20 degrees
+  beforeEach (function(){
+    thermostat = new Thermostat();
+  });
+
+
 
   it('initializes at 20 degrees', function() {
-    thermostat = new Thermostat();
     expect(thermostat.temperature).toEqual(20);
+  });
+
+  // You can increase the temperature with the up button
+
+  it('increases the temperature by 1 degree when you press the up button',function(){
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(21);
   });
 });
