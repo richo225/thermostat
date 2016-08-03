f488b6dd680aaa0ab6e267cca828509f0a7471df
Richard Bates <richo225@gmail.com>
2016-08-03 12:21:58 +0100

    Add power_saving attribute and switch

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -21,4 +21,26 @@ describe ('Thermostat', function(){
     expect(thermostat.temperature()).toEqual(19);
   });
 
+  it('returns error if temperature is below 10 degrees C', function(){
+    thermostat.current_temp = 10
+    expect(function(){ thermostat.decrease_temp() }).toThrowError('Temperature cannot go lower than 10ºC')
+  });
+
+  it('returns error if temrperature is above 25 degrees C', function(){
+    thermostat.current_temp = 25
+    expect(function(){ thermostat.increase_temp() }).toThrowError('Temperature cannot go higher than 25ºC')
+  });
+
+  describe ('power saving mode', function(){
+    it('is on by default', function(){
+      expect(thermostat.power_saving).toBe(true);
+    });
+
+    it('can be switched off', function(){
+      thermostat.power_saving_switch()
+      expect(thermostat.power_saving).toBe(false);
+    });
+
+  });
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,6 +3,7 @@
 function Thermostat(){
   this._DEFAULT_TEMPERATURE = 20;
   this.current_temp = this._DEFAULT_TEMPERATURE;
+  this.power_saving = true
 }
 
 Thermostat.prototype.temperature = function(){
@@ -10,9 +11,24 @@ Thermostat.prototype.temperature = function(){
 };
 
 Thermostat.prototype.increase_temp = function(){
+  if (this.current_temp === 25) {
+    throw new Error('Temperature cannot go higher than 25ºC');
+  }
   return this.current_temp += 1;
 };
 
 Thermostat.prototype.decrease_temp = function(){
+  if (this.current_temp === 10) {
+    throw new Error('Temperature cannot go lower than 10ºC');
+  }
   return this.current_temp -= 1;
+
+};
+
+Thermostat.prototype.power_saving_switch = function() {
+  if (this.power_saving === true) {
+    this.power_saving = false;
+  } else {
+    this.power_saving = true;
+  };
 };
