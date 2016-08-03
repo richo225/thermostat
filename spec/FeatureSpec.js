505c1da2f2e7291a58b247d41f01d8a5a84604fe
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-03 12:24:20 +0100

    Adds comments to FeatureSpec (change_by)

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -13,6 +13,7 @@ describe('Thermostat', function(){
   });
 
   // You can increase the temperature with the up button
+  // USE CHANGE_BY (OR EQUIVALENT) SO WE DON'T HARDCODE 21
 
   it('increases the temperature by 1 degree when you press the up button',function(){
     thermostat.up();
@@ -20,7 +21,8 @@ describe('Thermostat', function(){
   });
 
   // You can decrease the temperature with the down button
-  it('', function(){
+    // USE CHANGE_BY (OR EQUIVALENT) SO WE DON'T HARDCODE 19
+  it('decreases the temperature by 1 degree when you press the down button', function(){
     thermostat.down();
     expect(thermostat.temperature).toEqual(19);
   });
