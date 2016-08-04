7039070e63b3f3abcc8198b55dd920c84aed78f4
Sara <sarah.alalaya@gmail.com>
2016-08-04 15:32:33 +0300

    Challenge8: APIs done

--- src/jquery.js
+++ src/jquery.js
@@ -41,4 +41,22 @@ $('#powersaving-off').click(function() {
   $('#power-saving-status').text('OFF');
   updateTemperature();
 });
+
+$('#select-city').submit(function(event){
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
+});
+
+function displayWeather(city) {
+  var url   = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=267264c5f0acf7f84238123331b10e7a'
+  var units = '&units=metric'
+  $.get(url + token + units , function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });
+}
+
+
+
 });
